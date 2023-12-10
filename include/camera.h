2099d2f54a85018a67fb009c54d8066b2e2a6215
Bastien Sagetat <sagetatbastien@gmail.com>
2023-12-10 14:26:04 +0100

    Create camera (libgphoto3 wrapper) source files, and integrate it into main.c

---
+++ include/camera.h
@@ -0,0 +1,91 @@
+/*
+ * File: camera.h
+ * Description: Essentially a libgphoto2 C to C++ wrapper.
+ * Created on: 10 December 2023
+ */
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include <gphoto2/gphoto2.h>
+#include <string>
+#include <filesystem>
+
+namespace camera
+{
+    typedef void (* ContextErrorFunc) (const char *text);
+    typedef void (* ContextStatusFunc)(const char *text);
+    typedef struct _Camera GPCamera;
+
+    /**
+     * \class Context
+     * Camera context. Provides callbacks for error and status handling.
+     */
+    class Context
+    {
+    public:
+        Context(ContextErrorFunc error_func, ContextStatusFunc status_func);
+        ~Context();
+    private:
+        GPContext *gp_context_;
+        ContextErrorFunc error_func_;
+        ContextStatusFunc status_func_;
+        Context(Context const&)        = delete;
+        void operator=(Context const&) = delete;
+        friend void ContextErrorHandler(GPContext *gp_context, const char *str, void *data);
+        friend void ContextStatusHandler(GPContext *gp_context, const char *str, void *data);
+        friend class Camera;
+    };
+
+    /**
+     * \class File
+     * Camera file.
+     */
+    class File
+    {
+    public:
+        File();
+        ~File();
+        /**
+		 * \brief Save a camera file (most probably an image) to the local file system
+         */
+        void Save(const std::filesystem::path &file_path) const;
+    private:
+        CameraFile *gp_file_;
+        File(File const&)           = delete;
+        void operator=(File const&) = delete;
+        friend class Camera;
+    };
+
+    /**
+     * \class Camera
+     * Camera. Provides camera functions to take pictures, or to get a preview for instance.
+     */
+    class Camera
+    {
+    public:
+        Camera(const Context &context);
+        ~Camera();
+        /**
+		 * \brief Initialize the connected camera. Should be called first.
+         */
+        void Init();
+        /**
+		 * \brief Gets the summary of the connected camera.
+         */
+        std::string Summary() const;
+        /**
+		 * \brief Capture a preview image from the camera.
+         */
+        void CapturePreview(const File &file) const;
+
+    private:
+        const Context &context_;
+        GPCamera *gp_camera_;
+        bool initialized_;
+        Camera(File const&)           = delete;
+        void operator=(Camera const&) = delete;
+    };
+}
+
+#endif // !defined(CAMERA_H_)
