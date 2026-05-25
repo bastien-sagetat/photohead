5625c1e62247498800bc7b9c92d002ab200080df
Bastien Sagetat <bastien.sagetat.dev@proton.me>
2026-05-25 17:55:32 +0200

    software: bridge: Add header to bridge.py

---
+++ software/bridge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Filename: bridge.py
+Author: Bastien Sagetat
+Description:
+    Provide a network bridge (websocket, video streaming over RTSP)
+    to access stepper motor controller (pyserial) and camera (gphoto2) remotly.
+    GUI (Client) <-> Bridge (Server) <-> stepper controller / camera
+
+License: MIT License
+"""
